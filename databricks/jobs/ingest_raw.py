6cb7df0988c694c3cab4437111fe5951314ed092
Renan Marx <renanmarx@icloud.com>
2026-01-12 02:14:25 -0300

    Setting up black run

--- databricks/jobs/ingest_raw.py
+++ databricks/jobs/ingest_raw.py
@@ -5,21 +5,14 @@ process_date = dbutils.widgets.get("process_date")
 
 input_path = f"s3a://meu-bucket/dataset_x/raw/date={process_date}/"
 
-df = (
-    spark.read
-    .option("header", "true")
-    .csv(input_path)
-)
+df = spark.read.option("header", "true").csv(input_path)
 
-df = (
-    df
-    .withColumn("process_date", lit(process_date))
-    .withColumn("ingestion_ts", current_timestamp())
+df = df.withColumn("process_date", lit(process_date)).withColumn(
+    "ingestion_ts", current_timestamp()
 )
 
 (
-    df.write
-    .mode("overwrite")
+    df.write.mode("overwrite")
     .partitionBy("process_date")
     .format("delta")
     .saveAsTable("bronze.dataset_x")
--- databricks/jobs/ingest_silver.py
+++ databricks/jobs/ingest_silver.py
@@ -3,17 +3,12 @@ import utils.functions as f
 dbutils.widgets.text("process_date", "")
 process_date = dbutils.widgets.get("process_date")
 
-df = spark.table("bronze.dataset_x") \
-          .filter(col("process_date") == process_date)
+df = spark.table("bronze.dataset_x").filter(col("process_date") == process_date)
 
-df_clean = (
-    df.dropDuplicates(["id"])
-      .withColumn("valor", col("valor").cast("double"))
-)
+df_clean = df.dropDuplicates(["id"]).withColumn("valor", col("valor").cast("double"))
 
 (
-    df_clean.write
-    .mode("overwrite")
+    df_clean.write.mode("overwrite")
     .partitionBy("process_date")
     .format("delta")
     .saveAsTable("silver.dataset_x")
