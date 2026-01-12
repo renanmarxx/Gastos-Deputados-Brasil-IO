6c85dec6179ef7de670728b5c8272774db84f3f4
Renan Marx <renanmarx@icloud.com>
2026-01-12 02:39:20 -0300

    Inserting S3 Public link

--- databricks/jobs/ingest_raw.py
+++ databricks/jobs/ingest_raw.py
@@ -1,9 +1,16 @@
 from pyspark.sql.functions import lit, current_timestamp, col
+from datetime import datetime
 
 dbutils.widgets.text("process_date", "")
 process_date = dbutils.widgets.get("process_date")
 
-input_path = f"s3a://meu-bucket/dataset_x/raw/date={process_date}/"
+if not process_date:
+    process_date = datetime.now().strftime("%Y-%m-%d")
+
+process_date_str = str(process_date)
+
+#input_path = f"s3a://meu-bucket/dataset_x/raw/date={process_date}/"
+input_path = f"https://renan-marx-data-engineering-projects.s3.us-east-2.amazonaws.com/gastos-deputados-brasil-io/landing-bucket-gastos-deputados-brasil-io/dt%3D{process_date_str}/gastos-deputados_cota_parlamentar.csv.gz"
 
 df = spark.read.option("header", "true").csv(input_path)
 
