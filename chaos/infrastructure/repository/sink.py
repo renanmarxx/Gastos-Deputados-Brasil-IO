df13ef4ae081c73bffa20cf2ddb92fa8fab70741
Renan Marx <renanmarx@icloud.com>
2026-03-10 19:35:19 -0300

    feat/uploading chaos.infrastructure.repository.sink.py file

--- chaos/infrastructure/repository/sink.py
+++ chaos/infrastructure/repository/sink.py
@@ -26,25 +26,25 @@ class DataSink:
     def build_path(self, database: str) -> str:
         schema_environment = SchemaUtils.get_catalog_schema_environment(self.schema)
         base_path = (
-            f"{self.catalog_dataset.s3_bucket_name}{{separator}{schema_environment}}"
+            f"{self.catalog_dataset.s3_bucket_name}{{separator}}{schema_environment}"
         )
         match database:
             case "<PENDING>":
-                return base_path.format(separator = "/")
+                return base_path.format(separator="/")
             case "<PENDING>":
-                return base_path.format(separator = "/current/")
+                return base_path.format(separator="/current/")
             case _:
                 msg = f"Database should be '<PENDING' or '<PENDING>'. Got {database}."
                 logger.error(msg)
                 raise Exception(msg)
-            
+
     def first_load_to_edl(
-            self, df: DataFrame, delta_table_name: str, edl_path: str
+        self, df: DataFrame, delta_table_name: str, edl_path: str
     ) -> None:
-        
+
         if EDLHelper.catalog_table_exists(self.spark, delta_table_name):
             return
-        
+
         logger.info(f"Creating empty schema for {delta_table_name}")
 
         empty_df = EDLHelper.create_empty_dataframe(self.spark, df.schema)
@@ -52,33 +52,34 @@ class DataSink:
         schema_catalog_name = SchemaUtils.get_catalog_schema_full_name(self.schema)
 
         EDLHelper.write_empty_schema(
-            df = empty_df,
-            edl_table = delta_table_name,
-            schema_path = edl_path,
-            partition_columns = partitions_cols,
+            df=empty_df,
+            edl_table=delta_table_name,
+            schema_path=edl_path,
+            partition_columns=partitions_cols,
         )
         EDLHelper.alter_edl_properties(
-            spark = self.spark,
-            delta_table_name = delta_table_name,
-            catalog_full_name = schema_catalog_name,
-            dataset_environment = self.catalog_datset.environment_name,
+            spark=self.spark,
+            delta_table_name=delta_table_name,
+            catalog_full_name=schema_catalog_name,
+            dataset_environment=self.catalog_datset.environment_name,
         )
 
-    def append_data(self, df: DataFrame, database: str, namespace: str = "<PENDING>") -> None:
+    def append_data(
+        self, df: DataFrame, database: str, namespace: str = "<PENDING>"
+    ) -> None:
 
-        edl_path = self.build_path(database = database)
+        edl_path = self.build_path(database=database)
         versioned_table_name = SchemaUtils.get_versioned_table_name(self.schema)
         delta_table_name = f"{namespace}.{database}.{versioned_table_name}"
 
         edl_df = df.drop(
-            settings.spark.OPERATION_COLUMN_NAME,
-            settings.spark.ORDER_COLUMN_NAME
+            settings.spark.OPERATION_COLUMN_NAME, settings.spark.ORDER_COLUMN_NAME
         )
 
         self.first_load_to_edl(
-            df = edl_df,
-            delta_table_name = delta_table_name,
-            edl_path = edl_path,
+            df=edl_df,
+            delta_table_name=delta_table_name,
+            edl_path=edl_path,
         )
 
         logger.info(f"Writing data to {database.upper()}: {delta_table_name}")
@@ -87,7 +88,7 @@ class DataSink:
         edl_df.write.format("delta").mode("append").save(edl_path)
 
         logger.info(f"Data wrote into {database.upper()} table {delta_table_name}")
-    
-    #def merge_data(self, df: DataFrame, database: str, namespace: str = "<PENDING>") -> None:
 
-        # to be continued....
+    # def merge_data(self, df: DataFrame, database: str, namespace: str = "<PENDING>") -> None:
+
+    # to be continued....
