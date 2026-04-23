a6d5028dcdc5cef90091acf9b6a2348fd7d67044
Renan Marx <renanmarx@icloud.com>
2026-04-22 23:09:07 -0300

    chore: uploading imports

--- scripts/ingest_csv_to_s3.py
+++ scripts/ingest_csv_to_s3.py
@@ -7,7 +7,6 @@ from typing import Optional
 import boto3
 from brasil_io import BrasilIO
 
-from chaos.helpers.logging import logger
 from config import BRASIL_IO_TOKEN, S3_BUCKET, S3_PREFIX
 
 
@@ -52,10 +51,10 @@ def ingest_brasil_io_to_s3(
     out_path = os.path.join("data", f"{dataset_slug}_{table_name}.csv")
     with open(out_path, mode="wb") as fobj:
         fobj.write(csv_content)
-    logger.info(f"\nFile stored successfully at: {out_path}")
+    print(f"\nFile stored successfully at: {out_path}")
 
     # Uploading .csv file to the S3 Bucket
-    logger.info("\nStarting S3 Bucket upload...")
+    print("\nStarting S3 Bucket upload...")
 
     try:
         csv_bytes = BytesIO(csv_content)
@@ -66,10 +65,10 @@ def ingest_brasil_io_to_s3(
         key = f"{s3_prefix}/dt={today}/{dataset_slug}_{table_name}.csv"
 
         s3.upload_fileobj(csv_bytes, s3_bucket, key)
-        logger.info(f"Successful upload to the S3 Bucket: s3://{s3_bucket}/{key}")
+        print(f"Successful upload to the S3 Bucket: s3://{s3_bucket}/{key}")
 
     except Exception as e:
-        logger.error(f"Error uploading to the S3 Bucket: {type(e).__name__}: {e}")
+        print(f"Error uploading to the S3 Bucket: {type(e).__name__}: {e}")
         raise
 
 
