72e1b69c70c59196daa2009514955a81238342dc
Renan Marx <renanmarx@icloud.com>
2026-05-13 00:30:18 -0300

    feat:including terraform to validate files

--- terraform/modules/source_system_job/versions.tf
+++ terraform/modules/source_system_job/versions.tf
@@ -1,8 +1,9 @@
 terraform {
-    required_providers {
-        databricks = {
-            source = "databricks/databricks"
-            version = "1.25.0"
-        }
+  required_providers {
+    databricks = {
+      source  = "databricks/databricks"
+      version = "1.25.0"
     }
+  }
+  required_version = ">= 1.0"
 }
