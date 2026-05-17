5d3ce39df4bbcebd9aa7662fe920b05a1fa8b063
Renan Marx <renanmarx@icloud.com>
2026-05-17 16:55:41 -0300

    feat:including databricks and dlt helpers

---
+++ scripts/helpers/databricks.py
@@ -0,0 +1,118 @@
+from argparse import ArgumentParser
+
+from scripts.helpers.logging import logger
+
+
+class DatabricksHelper:
+    @staticmethod
+    def move_file(
+        dbutils, from_path: str, to_path: str, recursive: bool = False
+    ) -> bool:
+        """
+        Moves a file from the source path to the destinatiion path. If recursive is True, it will move all files and directories under the source path to the destination path.
+
+        Args:
+            dbutils: The Databricks utilities object.
+            from_path: The source path of the file to be moved.
+            to_path: The destination path where the file should be moved.
+            recursive: If True, move all files and directories under the source path to the destination path
+
+        Returns:
+            bool: True if the file was moved successfully, False otherwise.
+        """
+        logger.info(f"Moving file from {from_path} to {to_path}")
+        return dbutils.fs.mv(from_path, to_path, recursive)
+
+    @staticmethod
+    def list_files(dbutils, dir_path: str) -> list[str]:
+        """
+        Retrieves a list of files to process for the given table.
+
+        Args:
+            dbutils: The dbutils object for interacting with the file system.
+            dir_path: The directory path to list files from.
+        Returns:
+            list[str]: A list of file paths in the specified directory.
+        """
+        logger.info(f"Listing files in directory: {dir_path}")
+        return [file.path for file in dbutils.fs.ls(dir_path)]
+
+    @staticmethod
+    def directory_exists(dbutils, dir_path: str) -> bool:
+        """
+        Checks if the raw folder exists in a specified directory.
+
+        Args:
+            dbutils: The dbutils object for interacting with the file system.
+            dir_path: The directory path to check for existence.
+
+        Returns:
+            bool: True if the directory exists, False otherwise.
+        """
+        try:
+            dbutils.fs.ls(dir_path)
+            logger.info(f"Directory exists: {dir_path}")
+        except Exception as e:
+            if "java.io.FileNotFoundException" in str(e):
+                logger.error(f"Directory does not exist: {dir_path}")
+                return False
+
+    @staticmethod
+    def get_secret(dbutils, secret_scope: str, secret_key: str) -> str:
+        """
+        Retrieves a secret value from the Databricks secret scope.
+
+        Args:
+            dbutils: The dbutils object for accessing secrets.
+            secret_scope: The name of the secret scope.
+            secret_key: The key of the secret to retrieve.
+
+        Returns:
+            str: The value of the secret.
+        """
+        logger.info(f"Retrieving secret from scope: {secret_scope}, key: {secret_key}")
+        return dbutils.secrets.get(secret_scope, secret_key)
+
+    @staticmethod
+    def get_script_run_parameters(
+        expected_job_params: list = ["environment", "data_contracts"],
+    ) -> dict[str, str | list[str]]:
+        """
+        Retrieves the parameters passed to the Databricks job run.
+
+        Args:
+            expected_job_params: A list of expected parameter keys to retrieve from the job run parameters.
+
+        Returns:
+            dict[str, str | list[str]]: A dictionary containing the retrieved parameters and their values.
+        """
+        parser = ArgumentParser()
+        parser.add_argument(
+            "-e",
+            "--environment",
+            required=True,
+            help="The environment for the pipeline to run. Must be 'dev' or 'prod'.",
+        )
+        parser.add_argument(
+            "-c",
+            "--data-contracts",
+            nargs="+",
+            required=True,
+            help="Data contracts to use on the pipeline.",
+        )
+        parser.add_argument(
+            "-l",
+            "--landing_path",
+            required=False,
+            help="Landing path which files are ingested from. Used in pipelines that move files.",
+        )
+        args = parser.parse_args()
+
+        job_params = {
+            param_name: getattr(args, param_name)
+            for param_name in expected_job_params
+            if hasattr(args, param_name)
+        }
+
+        logger.info(f"Retrieved job parameters: {job_params}")
+        return job_params
---
+++ scripts/helpers/dlt.py
@@ -0,0 +1,165 @@
+from re import compile
+from pyspark.sql.types import StructType
+from pyspark.sql import Dataframe, SparkSession
+
+from scripts.helpers.logging import logger
+from scripts.core.settings import SparkSettings
+
+
+class DLTHelper:
+    @staticmethod
+    def order_files_to_process(input_list: list[str]) -> list[str]:
+        """
+        Orders the input list of files to process.
+
+        The function sorts the input list in ascending order and checks if there are any full load files.
+        If there are full load files, it moves them to the beggining of the list, preserving the order of the remaining files.
+
+        Args:
+            input_list (list[str]): A list of file paths to be ordered.
+
+        Returns:
+            list: The ordered list of files to process.
+        """
+        input_list = sorted(input_list)
+
+        # Checking if there are any full load files
+        load_index, load_pattern = (
+            0,
+            compile(r"LOAD\d+\." + f"{SparkSettings.RAW_FILE_TYPE}"),
+        )
+        for index, value in enumerate(input_list):
+            if load_pattern.match(value):
+                load_index = index
+                return input_list[load_index:] + input_list[:load_index]
+
+        return input_list
+
+    @staticmethod
+    def catalog_table_exists(spark: SparkSession, table: str) -> bool:
+        """
+        Checks if a table exists in the Spark catalog.
+
+        Args:
+            spark (SparkSession): The SparkSession object.
+            table (str): The name of the table to check for existence.
+
+        Returns:
+            bool: True if the table exists, False otherwise.
+        """
+
+        logger.info(f"Checking if table {table} exists in the Spark catalog")
+        return spark.catalog._jcatalog.tableExists(table)
+
+    @staticmethod
+    def create_empty_dataframe(spark: SparkSession, schema: StructType) -> Dataframe:
+        """
+        Creates an empty DataFrame with the specified schema.
+
+        Args:
+            spark (SparkSession): The SparkSession object.
+            schema (StructType): The schema for the empty DataFrame.
+
+        Returns:
+            DataFrame: An empty DataFrame with the specified schema.
+        """
+        logger.info("Creating an empty DataFrame with the specified schema")
+        return spark.createDataFrame(data=[], schema=schema)
+
+    @staticmethod
+    def write_empty_schema(
+        df: Dataframe,
+        dlt_table_name: str,
+        schema_path: str,
+        partition_columns: list[str],
+    ) -> None:
+        """
+        Writes an empty DataFrame with the specified schema to a Delta Live Table.
+
+        Args:
+            df (DataFrame): The empty DataFrame to write.
+            dlt_table_name (str): The name of the Delta Live Table to write to.
+            schema_path (str): The path where the schema of the DataFrame is stored.
+            partition_columns (list[str]): A list of columns to partition the Delta Live Table by.
+
+        Raises:
+            Exception: If the DataFrame is not empty.
+        """
+        if bool(df.count()):
+            raise Exception("DataFrame must be empty")
+
+        logger.info(
+            f"Writing an empty DataFrame with the specified schema to the Delta Live Table: {dlt_table_name}"
+        )
+        df_writer = df.write.format("delta").mode("overwrite")
+
+        if partition_columns:
+            logger.info(
+                f"Partitioning Delta Live Table by columns: {partition_columns}"
+            )
+            df_writer = df_writer.partitionBy(partition_columns)
+
+        df_writer.option("path", schema_path).saveAsTable(dlt_table_name)
+
+    @staticmethod
+    def join_condition_str(
+        primary_keys: list,
+        table_a_alias: str = "original",
+        table_b_alias: str = "updates",
+    ) -> str:
+        """
+        Generate the join condition string for two tables based on their primary keys.
+
+        Args:
+            primary_keys (list): A list of primary key column names to use in the join condition.
+            table_a_alias (str): The alias for the first table in the join condition. Default is "original".
+            table_b_alias (str): The alias for the second table in the join condition. Default is "updates".
+
+        Returns:
+            str: A string representing the join condition for the two tables.
+
+        Example:
+            >>> primary_keys = ["id", "name"]
+            >>> join_condition_str(primary_keys)
+            "original.id = updates.id AND original.name = updates.name"
+        """
+        logger.info(
+            f"Generating join condition string for primary keys: '{table_a_alias}'"
+        )
+        return " AND ".join(
+            [f"{table_a_alias}.{key} = {table_b_alias}.{key}" for key in primary_keys]
+        )
+
+    @staticmethod
+    def alter_table_properties(
+        spark: SparkSession,
+        dlt_table_name: str,
+        catalog_full_name: str,
+        dataset_environment: str,
+    ) -> None:
+        """
+        Alters the properties of a Delta Live Table in the specified database and schema.
+
+        Args:
+            spark (SparkSession): The SparkSession object.
+            database (str): The name of the database.
+            schema (CatalogSchema): The schema object.
+            dataset_environment (str): The dataset environment name.
+        """
+        logger.info(
+            f"Run ALTER TABLE PROPERTIES for the Delta Live Table: {dlt_table_name}"
+        )
+        sql_tblproperties = f"""
+            ALTER TABLE {dlt_table_name}
+            SET TBLPROPERTIES (
+                'delta.minReaderVersion' = '2',
+                'delta.minWriterVersion' = '5',
+                'delta.minReaderVersion' = '2',
+                'delta.enableChangeDataFeed' = 'true',
+                'quality' = 'bronze',
+                'environment' = '{dataset_environment}',
+                'source' = '{catalog_full_name}'
+
+            )
+        """
+        spark.sql(sql_tblproperties)
